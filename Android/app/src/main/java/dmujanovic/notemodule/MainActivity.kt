f6d9d6e6a2d691fa81f630212cf52362b566fe37
Dominik Mujanović <dmujanovi@foi.hr>
2023-02-11 17:48:23 +0100

    Fragmenti
    Fragmenti za dodavanje/prikaz bilješki; xml. fragmenta za dodavanje bilješki

--- Android/app/src/main/java/dmujanovic/notemodule/MainActivity.kt
+++ Android/app/src/main/java/dmujanovic/notemodule/MainActivity.kt
@@ -2,10 +2,33 @@ package dmujanovic.notemodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dmujanovic.notemodule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(DisplayNotes())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.addNotes -> replaceFragment(AddNotes())
+                R.id.displayNotes -> replaceFragment(DisplayNotes())
+                else -> {
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
