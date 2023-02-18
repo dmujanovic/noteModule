696f806cec50438646f6e95f59f57e34a64d9fb4
Dominik Mujanović <dmujanovi@foi.hr>
2023-02-19 00:09:30 +0100

    dosta zezancije
    Dohvaćanje iz baze - asinkronost riješena callbackom;
    Gradle modul -> maknut application plugin
    Spinner za odabir modula;

--- Android/app/src/main/java/dmujanovic/notemodule/DisplayNotes.kt
+++ Android/app/src/main/java/dmujanovic/notemodule/DisplayNotes.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import dmujanovic.core.NoteDisplayer
+import dmujanovic.listdisplay_module.ListNoteDisplayer
 
 
 class DisplayNotes : Fragment() {
+
+    var noteDisplayers: List<NoteDisplayer> = listOf(
+        ListNoteDisplayer()
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_display_notes, container, false)
+        val view = inflater.inflate(R.layout.fragment_display_notes, container, false)
+
+        val dr = DataRepository()
+
+        val spinner: Spinner = view.findViewById(R.id.spinner)
+        spinner.adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, noteDisplayers)
+
+
+
+        return view
     }
 
 }
